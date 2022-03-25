3c769145707644c5f854a92658a9a37539fe2262
Taz Uddin <tazuddinleton@gmail.com>
2022-03-25 17:23:06 +0600

    Find nth fibonacci number recursively and iteratively

--- chapter2/e-2.1.scala
+++ chapter2/e-2.1.scala
@@ -1,9 +1,21 @@
 
 object Fibonacci {
+    // fib recursive
     def fib(n: Int): Int = {
         if (n < 2) n
         else fib(n-1) + fib(n-2)
     }
+
+    def fibIter(x: Int, y: Int, position: Int, loopCounter: Int): Int = {
+      if (position < 2) position
+      else if (loopCounter > position) y
+      else fibIter(y, x+y, position, loopCounter+1)
+    }
+
+    def finFibIter(n: Int): Int = {
+        fibIter(0, 1, n, 2)
+    }
+
     // this method returns void but we do not have to 
     // write explicitly because it's a private method
     private def formatFib(n: Int) = {
@@ -16,5 +28,6 @@ object Fibonacci {
     // of public methods hence the Unit [Unit is equivalent of void in C like lang]
     def main(args: Array[String]): Unit = {        
         formatFib(8)
+        println(finFibIter(8))
     }
 }
