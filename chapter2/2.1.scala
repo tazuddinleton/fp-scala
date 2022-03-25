546ab22e3db1d730a1b59dab60a10aa406f039b8
Taz Uddin <tazuddinleton@gmail.com>
2022-03-25 17:37:07 +0600

    Higher order function

--- chapter2/2.1.scala
+++ chapter2/2.1.scala
@@ -9,12 +9,36 @@ object MyModule {
         else n
     }
 
+    def factorial(n: Int) = {
+        def go(n: Int, acc: Int): Int = {
+            if (n <= 0) acc
+            else go(n-1, n*acc)
+        }
+        go(n, 1)
+    }
+
     private def formatAbs(x: Int) = {
         val msg = "The absolute value of %d is %s"
         msg.format(x, abs(x))
     }
 
-    def main(args: Array[String]): Unit = {
-        println(formatAbs(abs(-3)))
+    private def formatFactorial(x: Int) = {
+        val msg = "The factorial of %d is %d"
+        msg.format(x, factorial(x))
     }
+
+    private def formatResult(name: String, x: Int, f: Int => Int) = {
+        var msg = "The %s of %d is %d"
+        msg.format(name, x, f(x))
+    }
+
+    def main(args: Array[String]): Unit = {
+        // println(formatAbs(abs(-3)))
+        // println(formatFactorial(5))
+
+        println(formatResult("factorial", 10, factorial))
+        println(formatResult("absolute", -10, abs))
+    }
+
+    
 }
