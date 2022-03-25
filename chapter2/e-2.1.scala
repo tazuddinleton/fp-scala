70f9a4bbd13d4da3d18c4ca03ede8ea07ee12fb2
Taz Uddin <tazuddinleton@gmail.com>
2022-03-25 17:26:03 +0600

    Refactor

--- chapter2/e-2.1.scala
+++ chapter2/e-2.1.scala
@@ -6,21 +6,26 @@ object Fibonacci {
         else fib(n-1) + fib(n-2)
     }
 
+    // this method returns void but we do not have to 
+    // write explicitly because it's a private method
+    private def formatFib(n: Int) = {
+        val str = "(recursive): the %d(th) position fib is: %d"
+        println(str.format(n, fib(n)))
+    }
+
     def fibIter(x: Int, y: Int, position: Int, loopCounter: Int): Int = {
       if (position < 2) position
       else if (loopCounter > position) y
       else fibIter(y, x+y, position, loopCounter+1)
     }
 
-    def finFibIter(n: Int): Int = {
+    def findFibIter(n: Int): Int = {
         fibIter(0, 1, n, 2)
     }
-
-    // this method returns void but we do not have to 
-    // write explicitly because it's a private method
-    private def formatFib(n: Int) = {
-        val str = "the %d(th) position fib is: %d"
-        println(str.format(n, fib(n)))
+    
+    private  def formatFibIter(n: Int) = {
+        val str = "(iterative): the %d(th) position fib is: %d"
+        println(str.format(n, findFibIter(n)))
     }
 
     //fib sequenc: 0 1 1 2 3 5
@@ -28,6 +33,6 @@ object Fibonacci {
     // of public methods hence the Unit [Unit is equivalent of void in C like lang]
     def main(args: Array[String]): Unit = {        
         formatFib(8)
-        println(finFibIter(8))
+        formatFibIter(8)
     }
 }
