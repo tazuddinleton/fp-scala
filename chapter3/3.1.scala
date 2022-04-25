e33d61cf8430d01e1e713ec905f1d258f711c941
Taz Uddin <tazuddinleton@gmail.com>
2022-04-26 01:26:23 +0600

    Do chapter 3 excersizes

--- chapter3/3.1.scala
+++ chapter3/3.1.scala
@@ -21,15 +21,83 @@ object List {
         case Cons(x, xs) => x * product(xs)
     }
 
+    // apply is idiomatic method that data structure have in their companion
+    // object to facilitate object creation. 
+    // A* = Seq[A]
+    // Seq has a head and a tail function
+    // as.tail: gives all but head as a Seq
+    // _* lets pass a Seq to a variadic method
     def apply[A](as: A*): List[A] = {
         if (as.isEmpty) Nil
-        else Cons(as.head, apply(as.tail : _*))
+        else Cons(as.head, apply(as.tail: _*)) 
     }
+
+    def tail[A](a: List[A]): List[A] = a match{
+        case Nil => Nil
+        case Cons(x, xs) => xs
+    }
+
+    def drop[A](l: List[A], n: Int): List[A] = {
+        if (n == 0) l
+        else drop(tail(l), n-1)
+    }
+
+    def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+        case Cons(h, t) if f(h) => dropWhile(t)(f)
+        case _ => l
+    }
+
+    def setHead[A](a: List[A], h: A): List[A] = a match {
+        case _ => Cons(h, tail(a))
+    }
+
+    def head[A](a: List[A]): A = a match {
+        case Nil => throw new Exception("Nil have no head")
+        case Cons(x, _) => x
+    }
+
+    def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+        case Nil => a2
+        case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
 
 
     def main(args: Array[String]): Unit = {
         println(List(1,2,3,4))
-        println(List.sum(List(1,2,3,4)))
-        println(List.product(List(1,2,3,4)))
+        println(sum(List(1,2,3,4)))
+        println(product(List(1,2,3,4)))
+
+        val m = List(1,2,3,4,5)
+        val x = m match {
+            case Cons(x,Cons(2, Cons(4, _))) => x
+            case Nil => 42
+            case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => sum(m)
+            // case Cons(h, t) => h + sum(t)
+            case _ => 101
+        }
+
+        println(x)
+
+        println(head(setHead(List(3,4,5), 10)))
+        val l = List(1,2,3,4,5,6,7)
+        println(dropWhile(l)(a => a < 5))
+        
+
+        val a1 = List(1,2,3,4)
+        val a2 = List(33,33,22)
+        println(append(a1, a2))
+
+        println(init(List(1,2,3,4,5)))
+    }
+
+
+    def init[A](l: List[A]): List[A] = {
+        def loop(l1: List[A], l2: List[A]): List[A] = l2 match {
+            case Nil => l1
+            case Cons(x, Nil) => l1
+            case Cons(x, xs) => loop(append(l1, Cons(head(l2), Nil)), tail(l2))
+        }
+        loop(Cons(head(l), Nil), tail(l))
     }
 }
